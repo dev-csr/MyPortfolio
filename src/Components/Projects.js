cebc6c6c33f6729faeceff950489a15973bf85e8
dev-csr <srikanthchilukuri01@gmail.com>
2024-10-10 15:46:42 +0530

    updated

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,10 +1,19 @@
 import "./Projects.css";
+import bankist from "./banklist.png";
+
 const Projects = ({ Projects_change }) => {
   return (
     <div id="projects" className={`projects_container ${Projects_change}`}>
       <div className="Projects">
         <div className="Project_img">
-          <img src="bankist.png" alt="bankist" />
+          <img className="project_images" src={bankist} alt="bankist" />
+          <a
+            className="project_links"
+            href="https://bankist-csrdev.netlify.app/"
+          >
+            {" "}
+            Bankist{" "}
+          </a>
         </div>
       </div>
     </div>
